47e795d401c876bf503e0f67addf74f94020d28b
FrankGu <frank@frankgu.club>
2023-07-10 01:06:30 +0800

    Add tensorboard and progress bar

--- test/test_train_vae.py
+++ test/test_train_vae.py
@@ -1,9 +1,12 @@
-import torch,sys,os
+import torch,sys,os,logging
 sys.path.append('.')
 sys.path.append('./models')
 sys.path.append('./dataset')
-
 # print(sys.path)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+from torch.utils.tensorboard import SummaryWriter
+writer = SummaryWriter(log_dir='log/vae')
+from tqdm import tqdm
 
 from models.vae import VAE
 from dataset.carla_topdown_dataset import CarlaTopDownDataset
@@ -18,7 +21,8 @@ def vae_loss(ori_x, con_x, mu, logvar):
 def vae_train(vae_model, vae_opt, loader,epoch):
     vae_model.train()
     for e in range(epoch):
-        for i, (x, _) in enumerate(loader):
+        scalar_name = 'epoch_%d' % e
+        for i, (x, _) in tqdm(enumerate(loader), total = len(loader)):
             x = x.to(device)
             vae_opt.zero_grad()
             con_x, mu, logvar = vae_model(x)
@@ -26,14 +30,15 @@ def vae_train(vae_model, vae_opt, loader,epoch):
             loss.backward()
             vae_opt.step()
             if i % 10 == 0:
-                print(f"Epoch {e}:{i}\t loss: {loss.item()}")
+                # print(f"Epoch {e}:{i}\t loss: {loss.item()}")
+                writer.add_scalar(scalar_name, loss.item(), i)
         torch.save(vae_model.state_dict(), f"vae_model_{e}.pth")
             
 if __name__ == "__main__":
     epoch = 50
-    batch_size = 2
+    batch_size = 8
     vae_model = VAE(1,1).to(device)
     optimizer = torch.optim.Adam(vae_model.parameters(), lr=1e-3)
-    ds = CarlaTopDownDataset('test/data')
+    ds = CarlaTopDownDataset('..\\..\\dataset')
     loader = torch.utils.data.DataLoader(ds, batch_size=batch_size, shuffle=True)
     vae_train(vae_model, optimizer, loader, epoch)
