3c9d7740235f45c0487766fcb0f94aa209e0fe32
FrankGu <frank@frankgu.club>
2023-07-16 20:24:59 +0800

    Add different weight to different object

--- dataset/carla_topdown_dataset.py
+++ dataset/carla_topdown_dataset.py
@@ -11,6 +11,7 @@ class CarlaTopDownDataset(BaseIODataset):
         weathers=[0,1, 3, 6, 8],
         towns=[1,2,3],
         onehot=True,
+        diff_weight = 100
         ):
         super().__init__(root)
         self.img_size = img_size
@@ -18,6 +19,7 @@ class CarlaTopDownDataset(BaseIODataset):
         pattern = re.compile('weather-(\d+).*town(\d\d)')
         self.route_frames = []
         self.onehot = onehot
+        self.diff_weight = diff_weight
         for line in dataset_indexs:
             if len(line.split()) != 2:
                 continue
@@ -55,6 +57,10 @@ class CarlaTopDownDataset(BaseIODataset):
         dtype = label.dtype
         shape = label.shape
         ones = torch.eye(N)
+        important = [4,19]
+        for i in important:
+            ones[i][i] = self.diff_weight
+        ones[6][6] = 10
         onehot = ones.index_select(0, label.int().view(-1)).reshape(*shape, N).to(dtype).squeeze(0).permute(2,0,1)
         return onehot
     
