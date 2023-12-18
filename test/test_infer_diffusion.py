647fefeea1767dc6897c2db10622376294ef2452
FrankGu <frank@frankgu.club>
2023-12-19 00:56:41 +0800

    Add half infer

--- test/test_infer_diffusion.py
+++ test/test_infer_diffusion.py
@@ -10,8 +10,9 @@ from dataset.carla_dataset import CarlaDataset
 from torchvision.transforms import ToTensor,Resize,InterpolationMode,CenterCrop,Normalize,Compose
 import clip
 from diffusers import PNDMScheduler
+# import pygame
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+half = False
 seg_tag = {
     0: [0,0,0], # Unlabeled
     1: [70,70,70], # Building
@@ -55,12 +56,12 @@ def check_path(path):
         os.makedirs(path)
     return path
 vae_model = VAE(26,26).to(device)
-vae_param = torch.load('pretrained/vae_one_hot/vae_model_61.pth')['model_state_dict']
+vae_param = torch.load('pretrained/vae_model/vae_model_60.pth',map_location=device)['model_state_dict']
 vae_model.load_state_dict(vae_param)
 vae_model.eval()
 
-UNet_model = UNet(with_lidar=True).to(device)
-UNet_param = torch.load('pretrained/diffusion_lidar/diffusion_model_9.pth',map_location=device)['model_state_dict']
+UNet_model = UNet(with_lidar=False).to(device)
+UNet_param = torch.load('pretrained/diffusion/diffusion_model_36.pth',map_location=device)['model_state_dict']
 UNet_model.load_state_dict(UNet_param)
 UNet_model.eval()
 scheduler = PNDMScheduler(
@@ -82,11 +83,15 @@ preprocess = Compose([
                 CenterCrop(224),
                 Normalize([0.48145466, 0.4578275, 0.40821073], [0.26862954, 0.26130258, 0.27577711]),
                 ])
-ds = CarlaDataset('/media/frank/sn640-0/dataset/dataset-val',weathers=[0,1,2,3,4,5,6,7,8,9,10,11,12,13],towns=[1,2,3,4,5,6,7,10],topdown_base_weight=1,topdown_diff_weight=100)
+if half:
+    vae_model.to(torch.bfloat16)
+    UNet_model.to(torch.bfloat16)
+ds = CarlaDataset('E:\\dataset-val',weathers=[0,1,2,3,4,5,6,7,8,9,10,11,12,13],towns=[1,2,3,4,5,6,7,10],topdown_base_weight=1,topdown_diff_weight=100)
 
 SAVE_NAME = "lidar@9"
 diff_step = 20
 save_path = os.path.join("test/infer", "_".join([SAVE_NAME, str(diff_step)]))
+torch.manual_seed(2023)
 check_path(save_path)
 check_path(os.path.join(save_path,"result"))
 check_path(os.path.join(save_path,"gt"))
@@ -117,7 +122,7 @@ for i,(data,label) in enumerate(ds):
                 preprocess(image_left), 
                 preprocess(image_right), 
                 preprocess(image_far)), dim=0)
-        lidar = data.lidar_2d_feature.unsqueeze(0).to(device)
+        # lidar = data.lidar_2d_feature.unsqueeze(0).to(device)
         # neg_lidar = torch.zeros_like(data.lidar_2d_feature.unsqueeze(0)).to(device)
         # lidar = torch.cat((neg_lidar,lidar),dim=0)
         vae_feature = label.vae_feature.unsqueeze(0).to(device)
@@ -126,6 +131,14 @@ for i,(data,label) in enumerate(ds):
         # out_clip_feature = torch.cat((neg_clip_feature,pos_clip_feature),dim=0).to(torch.float32)
         out_clip_feature = pos_clip_feature.to(torch.float32)
         out_vae = torch.randn(1,4,32,32).to(device)
+        if half:
+            out_vae = out_vae.to(torch.bfloat16)
+            out_clip_feature = out_clip_feature.to(torch.bfloat16)
+            vae_feature = vae_feature.to(torch.bfloat16)
+        else:
+            out_vae = out_vae.to(torch.float32)
+            out_clip_feature = out_clip_feature.to(torch.float32)
+            vae_feature = vae_feature.to(torch.float32)
         scheduler.set_timesteps(diff_step, device=device)
         start_time = time.time()
         for cur_time in scheduler.timesteps:
@@ -134,7 +147,7 @@ for i,(data,label) in enumerate(ds):
             # noise = torch.cat((out_vae,out_vae),dim=0)
             noise = out_vae
             noise = scheduler.scale_model_input(noise, cur_time)
-            pred_noise = UNet_model(out_vae=noise,lidar=lidar,out_encoder=out_clip_feature,time=cur_time_in)
+            pred_noise = UNet_model(out_vae=noise,out_encoder=out_clip_feature,time=cur_time_in)
             # pred_noise = pred_noise[1]
             # pred_noise = pred_noise[0] + 2 * (pred_noise[1] - pred_noise[0])
             out_vae = scheduler.step(pred_noise, cur_time,out_vae).prev_sample
@@ -152,10 +165,10 @@ for i,(data,label) in enumerate(ds):
         vae_gt = cvt_rgb_seg(vae_gt)
         
     cv2.imshow('topdown',out_vae)
-    cv2.imwrite(os.path.join(save_path,"result/%04d.png" %i),out_vae)
     cv2.imshow('topdown_gt',topdown_gt)
-    cv2.imwrite(os.path.join(save_path,"gt/%04d.png" %i),topdown_gt)
     cv2.imshow('vae_gt',vae_gt)
+    cv2.imwrite(os.path.join(save_path,"result/%04d.png" %i),out_vae)
+    cv2.imwrite(os.path.join(save_path,"gt/%04d.png" %i),topdown_gt)
     cv2.imwrite(os.path.join(save_path,"vae_gt/%04d.png" %i),vae_gt)
     # cv2.imshow('result',result)
     cv2.waitKey(1)
